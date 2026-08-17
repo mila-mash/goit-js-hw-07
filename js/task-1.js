e140739c8726c12b6c3d7331acee84cb75bd1b35
your_name <lsmaylik12@gmail.com>
2026-08-17 03:09:41 +0300

    1st corrections

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ console.log('Number of categories:', items.length);
 
 for (const item of items) {
   const category = item.querySelector('h2').textContent;
-  const elems = item.querySelectorAll('.categories-item').length;
+  const elems = item.querySelectorAll('ul > li').length;
 
   console.log('Category:', category);
   console.log('Elements:', elems);
